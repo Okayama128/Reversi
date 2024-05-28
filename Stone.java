7b0ff4907413246436748522bf0330ece1b1db4e
Okay23 <118034489+Okayama128@users.noreply.github.com>
2024-05-29 01:24:20 +0900

    Create Stone.java

---
+++ Stone.java
@@ -0,0 +1,28 @@
+package reversi;
+
+public class Stone {
+	private int status;
+	
+	public Stone() {}
+	
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public int getStatus() {
+		return status;
+	}
+	
+	public void reverse() {
+		switch(status) {
+			case Cons.WHITE:
+				status = Cons.BLACK;
+				break;
+			case Cons.BLACK:
+				status = Cons.WHITE;
+				break;
+			default:
+				System.out.println("Stone.reverse():Error");
+				break;
+		}
+	}
+}
