84e2bf29c1a1abc56d8ebca0634c4317b819331d
Okay23 <118034489+Okayama128@users.noreply.github.com>
2024-05-29 01:24:34 +0900

    Create Cons.java

---
+++ Cons.java
@@ -0,0 +1,21 @@
+package reversi;
+
+public class Cons {
+	// This is Constant Number Class
+	// Stone
+	public static final int BLACK = -1;
+	public static final int WHITE = 1;
+	public static final int POSSIBLE = -100;
+	// Stone Status
+	public static final int FULL = 100;
+	public static final int ZERO = 0;
+	// Direction
+	public static final int UP = 0;// Up
+	public static final int RU = 1;// Right Up
+	public static final int RT = 2; // Right
+	public static final int RD = 3;
+	public static final int DW = 4;
+	public static final int LW = 5;
+	public static final int LT = 6;
+	public static final int LU = 7;
+}
