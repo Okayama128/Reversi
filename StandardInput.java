13a48467a0b0fb9920b640260174793a42b6e42c
Okay23 <118034489+Okayama128@users.noreply.github.com>
2024-06-03 07:19:25 +0900

    Update StandardInput.java

--- StandardInput.java
+++ StandardInput.java
@@ -1,16 +1,15 @@
-package reversi;
+package reversi2;
 import java.util.Scanner;
 
 public class StandardInput {
+
+    static Scanner sc = new Scanner(System.in);
 	
 	public static int readInt() {
-		Scanner sc = new Scanner(System.in);
-		int result = sc.nextInt();
-		sc.close();
-		return result;
+		return sc.nextInt();
 	}
-//	public String readString(String str) {
-//		return str;
-//	}
+	public String readString() {
+		return sc.next();
+	}
 	
 }
